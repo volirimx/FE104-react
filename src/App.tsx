e722b31da31fafdd7f680ceab0553e5aec235cd9
YuriBY <vv0325056@gmail.com>
2023-08-01 11:21:11 +0300

    Tasks 1-2 are done

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
 import "./App.css";
-import { Button } from "./components/button";
+import { Button } from "./components/Button_primary/button";
+import { UserButton } from "./components/Users/userbutton";
 
 function App() {
   
 
   return (    
       <div>
+        <div>
     <Button content={'Primary'} mode={'primary'}/> 
     <Button content={'Secondary'} mode={'secondary'}/> 
-    <Button content={'Secondary2'} mode={'secondary2'}/>    
+    <Button content={'Secondary2'} mode={'secondary2'}/>     
       </div>
+    <div>
+       <UserButton firstName="Artem" lastName="Malkin"/>  
+    </div>
+      </div>
+      
   
  
   )
