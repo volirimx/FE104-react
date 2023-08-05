0deb0ecbdf6a32a4626016ae7d11d03f05d1d929
AnastasiaZhurkevich <sh_nastia95@mail.ru>
2023-08-05 20:20:43 +0300

    Nastia-lesson37

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import "./App.css";
+import { Button } from "./components/Button/index.tsx";
+import { User } from "./components/UserInfo/index.tsx";
 
 function App() {
-  return <div></div>;
+  return <div>
+    <div>
+      <Button content="Primary" mode="primary" />
+      <Button content="Secondary" mode="secondary" />
+      <Button content="Tertiary" mode="tertiary" disabled={true} />
+    </div>
+    <div>
+      <User name={"Artem Malkin"} />
+    </div>
+  </div>;
 }
 
 export default App;
