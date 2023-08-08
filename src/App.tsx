f9677a77ffd8785710092bb5406549e1b034a831
Aleksey <poznuak2020@yandex.by>
2023-08-08 12:47:01 +0300

    Aleksey-burger

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import { Header } from "./companents/Header/index.tsx";
 
 function App() {
-  return <div></div>;
+ return (
+  <>
+   <Header />
+  </>
+ );
 }
 
 export default App;
